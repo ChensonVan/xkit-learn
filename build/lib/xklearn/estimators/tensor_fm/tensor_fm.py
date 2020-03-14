9c429380d6b437373673e7cc365c7288a2dfaa5d
vane <chenson.van@gmail.com>
2020-03-14 19:55:31 +0800

    remove duplicate codes

--- build/lib/xklearn/estimators/tensor_fm/tensor_fm.py
+++ build/lib/xklearn/estimators/tensor_fm/tensor_fm.py
@@ -122,24 +122,24 @@ def train(train_dataset, num_factors=2, max_iter=1,
             
 
 
-def to_tf_dataset(X, y, dtype=tf.float32, batch_size=256, shuffle_buffer_size=None):
-    X = np.array(X)
-    y = np.array(y)
+# def to_tf_dataset(X, y, dtype=tf.float32, batch_size=256, shuffle_buffer_size=None):
+#     X = np.array(X)
+#     y = np.array(y)
     
-    if X.shape[0] != y.shape[0]:
-        raise ValueError('The number of training examples is not the same as the number of labels')
+#     if X.shape[0] != y.shape[0]:
+#         raise ValueError('The number of training examples is not the same as the number of labels')
         
-    dataset = tf.data.Dataset.from_tensor_slices((tf.cast(X, dtype=dtype), tf.cast(y, dtype=dtype))).batch(batch_size)
+#     dataset = tf.data.Dataset.from_tensor_slices((tf.cast(X, dtype=dtype), tf.cast(y, dtype=dtype))).batch(batch_size)
     
-    if shuffle_buffer_size:
-        dataset = dataset.shuffle(shuffle_buffer_size)
+#     if shuffle_buffer_size:
+#         dataset = dataset.shuffle(shuffle_buffer_size)
     
-    return dataset
+#     return dataset
     
     
-def to_tf_tensor(X, dtype=tf.float32):
-    X = np.array(X)
-    return tf.cast(X, dtype=dtype)
+# def to_tf_tensor(X, dtype=tf.float32):
+#     X = np.array(X)
+#     return tf.cast(X, dtype=dtype)
 
 
 class BaseFactorizationMachine(BaseEstimator):
