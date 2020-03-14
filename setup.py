567e1b0b30b56b54268531ebadb0635cf21a78d0
vane <chenson.van@gmail.com>
2020-03-15 00:29:55 +0800

    modified setup

--- setup.py
+++ setup.py
@@ -27,64 +27,15 @@ def read(fname):
 
 
 NAME = "xklearn"
-
-"""
-名字，一般放你包的名字即可
-"""
-
-PACKAGES = ["xklearn"]
-
-"""
-包含的包，可以多个，这是一个列表
-"""
-
 DESCRIPTION = "this is a test package for packing python liberaries tutorial."
-
-"""
-关于这个包的描述
-"""
-
 LONG_DESCRIPTION = read("README.md")
-
-"""
-参见read方法说明
-"""
-
 KEYWORDS = "test python package"
-
-"""
-关于当前包的一些关键字，方便PyPI进行分类。
-"""
-
 AUTHOR = "Chenson Van"
-
-"""
-谁是这个包的作者，写谁的名字吧
-"""
-
 AUTHOR_EMAIL = "chenson.van@gmail.com"
-
-"""
-作者的邮件地址
-"""
-
 URL = "http://www.chenson.cc"
-
-"""
-你这个包的项目地址，如果有，给一个吧，没有你直接填写在PyPI你这个包的地址也是可以的
-"""
-
 VERSION = "0.0.1"
-
-"""
-当前包的版本，这个按你自己需要的版本控制方式来
-"""
-
 LICENSE = "MIT"
 
-"""
-授权方式，我喜欢的是MIT的方式，你可以换成其他方式
-"""
 
 setup(
     name=NAME,
@@ -122,7 +73,7 @@ setup(
 
     include_package_data=True,
 
-#    package_dir={'mlearn':'mlearn'},
+   # package_dir={'xklearn':'xklearn'},
 
     zip_safe=True
 
